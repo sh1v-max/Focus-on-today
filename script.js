5b2f4973d42b402391ae76fad45e6bc1536bed54
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-12 17:03:07 +0530

    show error style

---
+++ script.js
@@ -0,0 +1,19 @@
+const checkBoxList = document.querySelectorAll('.custom-checkbox');
+const inputField = document.querySelectorAll('.goal-input');
+const errorLabel = document.querySelector('.error-label')
+const progressBar = document.querySelector('.progress-bar')
+
+checkBoxList.forEach((checkBox) => {
+  checkBox.addEventListener('click', (e) => {
+    const allGoalsAdded = [...inputField].every(function(input){
+      return input.value
+    })
+    if (allGoalsAdded) {
+      checkBox.parentElement.classList.toggle('completed')
+    }else{
+      // errorLabel.style.display = 'block'
+      progressBar.classList.add('')
+    }
+  } )
+})
+
