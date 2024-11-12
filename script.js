4174ef75b25a6535ec0793c2128260e9431e0e61
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-11-12 20:45:50 +0530

    tick style

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const checkBoxList = document.querySelectorAll('.custom-checkbox');
 const inputField = document.querySelectorAll('.goal-input');
 const errorLabel = document.querySelector('.error-label')
 const progressBar = document.querySelector('.progress-bar')
+const progressValue = document.querySelector('.progress-value')
 
 checkBoxList.forEach((checkBox) => {
   checkBox.addEventListener('click', (e) => {
@@ -10,6 +11,7 @@ checkBoxList.forEach((checkBox) => {
     })
     if (allGoalsAdded) {
       checkBox.parentElement.classList.toggle('completed')
+      progressValue.style.width = '33.333%'
     }else{
       // errorLabel.style.display = 'block'
       progressBar.classList.add('show-error')
